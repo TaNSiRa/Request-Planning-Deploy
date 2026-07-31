0d425855e8ed91a06ff88379cf48089e3554a139
TaNSiRa <tansirawit2543@gmail.com>
2026-07-31 14:19:14 +0700

    update 2026-07-31 14:16

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1511073130" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2516447391" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
