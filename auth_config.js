1a3690b4eea4073101dfc3ff6470a69c5fe6f58a
TaNSiRa <tansirawit2543@gmail.com>
2026-08-03 15:36:58 +0700

    update 2026-08-03 15:34

--- auth_config.js
+++ auth_config.js
@@ -1,5 +1,5 @@
 ﻿window.arAuthConfig = {
-  clientId: "b3b3b7fe-6802-45de-80b8-1ce46ac8851d",
+  clientId: "43e42089-2baf-4707-a472-cded7d15c334",
   tenantId: "84888232-8ee9-4d38-82b0-4529bd56be8a",
   scopes: "openid profile email User.Read"
 };
